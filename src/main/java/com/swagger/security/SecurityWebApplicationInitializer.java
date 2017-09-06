031dafd0712152d92b92a6d14fbde55f210b3424
wuy <wuy@yanxintec.com>
2017-09-06 19:23:17 +0800

    添加session基于redis共享

--- src/main/java/com/swagger/security/SecurityWebApplicationInitializer.java
+++ src/main/java/com/swagger/security/SecurityWebApplicationInitializer.java
@@ -1,11 +1,19 @@
 package com.swagger.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+
+import javax.servlet.ServletContext;
 
 /**
  * Created by wuy on 2017/9/5.
  */
+
+@Order(100)
 public class SecurityWebApplicationInitializer  extends AbstractSecurityWebApplicationInitializer {
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+    }
 }
